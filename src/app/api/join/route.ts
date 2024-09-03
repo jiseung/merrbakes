79dd2866fedc425a68548c9e82aacd9f95bdc01f
jiseung <24727548+jiseung@users.noreply.github.com>
2024-09-03 18:58:10 -0500

    emails subscribe to mailchimp list

--- src/app/api/join/route.ts
+++ src/app/api/join/route.ts
@@ -1,6 +1,16 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { promises as fs } from 'fs';
 import path from 'path';
+import mailchimp from "@mailchimp/mailchimp_marketing";
+
+const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY;
+const MAILCHIMP_LIST_ID = process.env.MAILCHIMP_LIST_ID;
+const MAILCHIMP_SERVER_PREFIX = process.env.MAILCHIMP_SERVER_PREFIX;
+
+mailchimp.setConfig({
+  apiKey: MAILCHIMP_API_KEY,
+  server: MAILCHIMP_SERVER_PREFIX,
+});
 
 export async function POST(req: NextRequest) {
   try {
@@ -14,15 +24,21 @@ export async function POST(req: NextRequest) {
 
     // Define the path where the email will be stored
     const filePath = path.join(process.cwd(), 'emails.txt');
-
     // Append the email to the file with a newline
     await fs.appendFile(filePath, `${email}\n`);
 
+    const response = await mailchimp.lists.addListMember(MAILCHIMP_LIST_ID, {
+      email_address: email,
+      status: 'subscribed'
+    });
+    if (response.status !== 200) {
+      console.log(response);
+    }
     // Return a success response
     return NextResponse.json({ success: true, message: 'Email saved successfully' });
   } catch (error) {
     const err = error as Error;
-    console.log(err.stack);
+    console.log(err);
     // Handle errors and return a failure response
     return NextResponse.json({ success: false, error: err.message }, { status: 500 });
   }
